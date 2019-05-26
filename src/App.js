dd9429acdcba6f658e5cfa394afc73b3c2124619
Mindaugas <mindaugas.bernotas@mediapark.com>
2019-05-26 15:21:46 +0300

    Add venue info component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './resources/styles.css';
 
 import Hedear from './components/header_footer/Header';
 import Featured from './components/featured/Featured';
+import VenueInfo from './components/venue_info/VenueInfo'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div className="App" style={{height:"1500px" , backgroundColor: 'red'}}>
         <Hedear/>
         <Featured/>
+        <VenueInfo/>
     </div>
   );
 }
