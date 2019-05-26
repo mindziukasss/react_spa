90d2e8b4735068b13c92af05577ce5a16b856626
Mindaugas <mindaugas.bernotas@mediapark.com>
2019-05-26 09:27:08 +0300

    Add header and fonts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './resources/styles.css';
+
+import Hedear from './components/header_footer/Header';
 
 
 function App() {
   return (
     <div className="App">
+        <Hedear/>
       Hello
     </div>
   );
