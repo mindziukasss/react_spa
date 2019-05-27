1349083e5ef7ef0beef8d33b42b4ccdbbc2b5d6e
Mindaugas <mindaugas.bernotas@mediapark.com>
2019-05-27 18:58:56 +0300

    Updates

---
+++ precache-manifest.4f5348ca3d6fd0065739d393d4f074a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4acde5d3c9e70e6d27ba617030185030",
+    "url": "/react_spa/index.html"
+  },
+  {
+    "revision": "1881a7c5650e4dc2c147",
+    "url": "/react_spa/static/css/main.d6fa7445.chunk.css"
+  },
+  {
+    "revision": "48ca1c70ae05f89d6c90",
+    "url": "/react_spa/static/js/2.a5a9c01c.chunk.js"
+  },
+  {
+    "revision": "1881a7c5650e4dc2c147",
+    "url": "/react_spa/static/js/main.c92a5115.chunk.js"
+  },
+  {
+    "revision": "848c3f658688f69b3928",
+    "url": "/react_spa/static/js/runtime~main.fb24aae3.js"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/react_spa/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/react_spa/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/react_spa/static/media/slide_two.3a3ffd36.jpg"
+  }
+]);
